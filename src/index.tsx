10d4bd18c6a47f6de73f4cd8c63aa3d3fa67fe1b
Quan Brew <t@ioover.net>
2018-08-24 15:51:32 +0800

    parcel

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import * as ReactDOM from 'react-dom';
 import Sheet from './Sheet';
 import './index.css';
 import 'bulma/css/bulma.css';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Sheet/>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+// registerServiceWorker();
